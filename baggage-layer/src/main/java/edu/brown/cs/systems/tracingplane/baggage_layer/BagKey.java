8ca12c02e262fc93c279706875e7da1d337dedbd
Jonathan Mace <jonathan.c.mace@gmail.com>
2016-12-08 09:41:26 -0500

    Add some comments

--- baggage-layer/src/main/java/edu/brown/cs/systems/tracingplane/baggage_layer/BagKey.java
+++ baggage-layer/src/main/java/edu/brown/cs/systems/tracingplane/baggage_layer/BagKey.java
@@ -3,38 +3,130 @@ package edu.brown.cs.systems.tracingplane.baggage_layer;
 import java.nio.ByteBuffer;
 import java.util.List;
 
-import com.google.common.collect.Lists;
-
 import edu.brown.cs.systems.tracingplane.atom_layer.types.Lexicographic;
 import edu.brown.cs.systems.tracingplane.baggage_layer.protocol.AtomPrefixes.AtomPrefix;
 import edu.brown.cs.systems.tracingplane.baggage_layer.protocol.AtomPrefixes.IndexedHeaderPrefix;
 import edu.brown.cs.systems.tracingplane.baggage_layer.protocol.AtomPrefixes.KeyedHeaderPrefix;
 import edu.brown.cs.systems.tracingplane.baggage_layer.protocol.BagKeySerialization;
 
+/**
+ * <p>
+ * A {@link BagKey} is used to look up data items and child bags within a
+ * baggage instance. A {@link BagPath} is a list of zero or more bag keys that
+ * acts much like a path in a file system.
+ * </p>
+ * 
+ * <p>
+ * BagKey instances should be created with the static methods {@link indexed}
+ * and {@link named}. As per the Baggage protocol, fields can either be named
+ * (using arbitrary bytes) or indexed (with an unsigned integer). Indexes for
+ * fields are statically assigned for efficiency in more established protocols.
+ * It is recommended to use named keys for ad-hoc data.
+ * </p>
+ */
 public abstract class BagKey implements Comparable<BagKey> {
 
+	/**
+	 * Zero or more BagKeys represent a path to data in baggage. Paths can be
+	 * used to access data from BaggageContents.
+	 */
 	public static class BagPath implements Comparable<BagPath> {
 
-		public final List<BagKey> keys;
+		public final BagKey[] keys;
 
-		public BagPath(BagKey... keys) {
-			this(Lists.newArrayList(keys));
+		private BagPath(BagKey[] keys) {
+			this.keys = keys;
 		}
 
-		public BagPath(List<BagKey> keys) {
-			this.keys = keys;
+		/**
+		 * Creates and returns a new path that joins the provided keys
+		 */
+		public static BagPath from(BagKey... keys) {
+			return new BagPath(keys);
+		}
+
+		/**
+		 * Creates and returns a new path that joins the provided keys
+		 */
+		public static BagPath from(List<BagKey> keys) {
+			return from(keys.toArray(new BagKey[keys.size()]));
+		}
+
+		/**
+		 * Returns a new BagPath that is equal to this path plus the provided
+		 * key appended to the end
+		 */
+		public BagPath append(BagKey key) {
+			BagKey[] newKeys = new BagKey[keys.length + 1];
+			System.arraycopy(keys, 0, newKeys, 0, keys.length);
+			newKeys[keys.length] = key;
+			return new BagPath(newKeys);
+		}
+
+		/**
+		 * Creates and appends a new indexed key to the end of this bag path.
+		 * Returns a new BagPath that is equal to this path plus the created key
+		 * appended to the end.
+		 */
+		public BagPath append(int index) {
+			return append(indexed(index));
+		}
+
+		/**
+		 * Creates and appends a new indexed key to the end of this bag path.
+		 * Returns a new BagPath that is equal to this path plus the created key
+		 * appended to the end.
+		 */
+		public BagPath append(int index, BagOptions options) {
+			return append(indexed(index, options));
+		}
+
+		/**
+		 * Creates and appends a new named key to the end of this bag path.
+		 * Returns a new BagPath that is equal to this path plus the created key
+		 * appended to the end.
+		 */
+		public BagPath append(ByteBuffer key) {
+			return append(named(key));
+		}
+
+		/**
+		 * Creates and appends a new named key to the end of this bag path.
+		 * Returns a new BagPath that is equal to this path plus the created key
+		 * appended to the end.
+		 */
+		public BagPath append(String key) {
+			return append(named(key));
+		}
+
+		/**
+		 * Creates and appends a new named key to the end of this bag path.
+		 * Returns a new BagPath that is equal to this path plus the created key
+		 * appended to the end.
+		 */
+		public BagPath append(ByteBuffer key, BagOptions options) {
+			return append(named(key, options));
+		}
+
+		/**
+		 * Creates and appends a new named key to the end of this bag path.
+		 * Returns a new BagPath that is equal to this path plus the created key
+		 * appended to the end.
+		 */
+		public BagPath append(String key, BagOptions options) {
+			return append(named(key, options));
 		}
 
 		@Override
 		public int compareTo(BagPath o) {
-			int size = Math.min(keys.size(), o.keys.size());
+			int size = Math.min(keys.length, o.keys.length);
 			for (int i = 0; i < size; i++) {
-				int comparison = keys.get(i).compareTo(o.keys.get(i));
+				int comparison = keys[i].compareTo(o.keys[i]);
 				if (comparison != 0) {
 					return comparison;
 				}
 			}
-			return Integer.compare(keys.size(), o.keys.size());
+			return Integer.compare(keys.length, o.keys.length);
 		}
 
 		@Override
@@ -101,6 +193,10 @@ public abstract class BagKey implements Comparable<BagKey> {
 		return new Keyed(name, options);
 	}
 
+	/**
+	 * An Indexed BagKey is one that uses an integer as identifier (versus
+	 * arbitrary bytes for a {@Keyed} key)
+	 */
 	public static final class Indexed extends BagKey {
 
 		private ByteBuffer byteRepr = null;
@@ -159,6 +255,10 @@ public abstract class BagKey implements Comparable<BagKey> {
 
 	}
 
+	/**
+	 * A Keyed BagKey is one that uses arbitrary bytes as identifier (versus an
+	 * integer for {@Indexed} key)
+	 */
 	public static final class Keyed extends BagKey {
 
 		private ByteBuffer byteRepr = null;
---
+++ baggage-layer/src/test/java/edu/brown/cs/systems/tracingplane/baggage_layer/protocol/TestAtomPrefixes.java
@@ -0,0 +1,14 @@
+package edu.brown.cs.systems.tracingplane.baggage_layer.protocol;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class TestAtomPrefixes extends TestCase {
+	
+	@Test
+	public void testDataTakesPriority() {
+		
+	}
+
+}
