7ba2e78409f178db71e299767660a2f09c4dc767
Jonathan Mace <jonathan.c.mace@gmail.com>
2016-12-12 15:18:41 -0500

    Start adding BaggageLayer documentation

--- baggage-layer/src/main/java/edu/brown/cs/systems/tracingplane/baggage_layer/BaggageLayer.java
+++ baggage-layer/src/main/java/edu/brown/cs/systems/tracingplane/baggage_layer/BaggageLayer.java
@@ -5,7 +5,21 @@ import java.util.List;
 import edu.brown.cs.systems.tracingplane.atom_layer.AtomLayer;
 import edu.brown.cs.systems.tracingplane.baggage_layer.BagKey.BagPath;
 
-//TODO: description and method documentation
+/**
+ * <p>
+ * The BaggageLayer is an implementation of the AtomLayer and protocol for storing data in Baggage atoms. The static
+ * methods in {@link BaggageContents} offer access to the data contained in a Baggage instance
+ * </p>
+ * 
+ * <p>
+ * The BaggageLayer protocol provides a generic, general-purpose map for user data. Data can be stored under keys or
+ * paths (e.g., "xtrace.taskId") and accessed accordingly. The BaggageLayer protocol defines how to translate between
+ * data stored in a map and data stored as atoms. The atom representation of Baggage is amenable to AtomLayer merging,
+ * such that two baggages with different values for a field will correctly merge agnostic to the semantics of the layer.
+ * </p>
+ *
+ * @param <B> Some implementation of {@link BaggageContents} used by this transit layer.
+ */
 public interface BaggageLayer<B extends BaggageContents> extends AtomLayer<B> {
 
     public boolean checkOverflow(B baggage, BagPath path, boolean includeChildren, boolean includeTrimmed);
