6aec242c197802df1af7c012c2e8c3b6c04e8c84
Jonathan Mace <jonathan.c.mace@gmail.com>
2017-04-09 23:34:08 -0400

    Add another compact call as I'm not sure on the correct semantics for this yet

--- baggage-buffers/src/main/java/edu/brown/cs/systems/tracingplane/baggage_buffers/BaggageBuffers.java
+++ baggage-buffers/src/main/java/edu/brown/cs/systems/tracingplane/baggage_buffers/BaggageBuffers.java
@@ -193,4 +193,18 @@ public class BaggageBuffers implements BaggageLayer<BaggageBuffersContents> {
         }
     }
 
+    
+    /**
+     * Optional operation.  Compacts datatypes in the current thread's baggage based on per-datatype compaction semantics.
+     * For most data types this does nothing; for CRDTs it collapses known instance states into a single instance.
+     */
+    public static Baggage compact(Baggage original, Baggage current) {
+        // TODO: provide implementation of compact.  Possibly push compact to transit layer.  Unsure yet.
+        if (isCompactionEnabled) {
+            return current;
+        } else {
+            return current;
+        }
+    }
+
 }
