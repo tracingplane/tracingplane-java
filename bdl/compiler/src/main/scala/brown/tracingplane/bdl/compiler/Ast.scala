6d20e3b4bfaad265b4cc927c933a0b541eef5b7b
Jonathan Mace <jonathan.c.mace@gmail.com>
2017-05-15 12:58:54 -0400

    Add BDL compiler

---
+++ bdl/compiler/src/main/scala/brown/tracingplane/bdl/compiler/Ast.scala
@@ -0,0 +1,171 @@
+package brown.tracingplane.bdl.compiler
+
+import scala.collection.JavaConverters._
+import scala.collection.mutable.Map
+import fastparse.all._
+import scala.reflect.runtime.universe._
+
+
+object Ast {
+
+  sealed trait FieldType {
+    def isValid: Boolean = true
+  }
+  
+  sealed abstract class ParameterizedType(parameters: List[FieldType]) extends FieldType {
+    def getParameters(): java.util.List[FieldType] = {
+      return parameters.asJava
+    }
+    override def toString(): String = {
+      return s"${this.getClass.getSimpleName}<${parameters.mkString(", ")}>"
+    }
+  }
+  
+  case class UserDefinedType(var packageName: String, name: String, var structType: Boolean = false) extends FieldType {
+    def isStructType(): Boolean = {
+      return structType
+    }
+    def hasPackageName(): Boolean = {
+      return packageName != null && !packageName.equals("")
+    }
+    override def toString(): String = {
+      if (hasPackageName()) {
+        return s"$packageName.$name"
+      } else {
+        return name
+      }
+    }
+  }
+  
+  sealed trait BuiltInType extends FieldType
+  
+  sealed trait PrimitiveType extends BuiltInType {
+    override def toString(): String = {
+      val className = this.getClass.getName
+      return className.substring(BuiltInType.getClass.getName.length(), className.length()-1)
+//      return this.getClass.getSimpleName // SI-2034
+    }
+  }
+  
+  object BuiltInType {
+    sealed trait Numeric extends PrimitiveType
+    sealed trait Float extends Numeric
+    sealed trait Signed extends Numeric
+    sealed trait Unsigned extends Numeric
+
+    case object taint extends Unsigned
+    case object bool extends Unsigned
+    case object int32 extends Unsigned
+    case object int64 extends Unsigned
+    case object sint32 extends Signed
+    case object sint64 extends Signed
+    case object fixed32 extends Unsigned
+    case object fixed64 extends Unsigned
+    case object sfixed32 extends Signed
+    case object sfixed64 extends Signed
+    case object float extends Float
+    case object double extends Float
+    
+    case object string extends PrimitiveType
+    case object bytes extends PrimitiveType
+    
+    case object Counter extends BuiltInType
+    
+    case class Set(of: FieldType) extends ParameterizedType(List[FieldType](of)) with BuiltInType {
+      override def isValid(): Boolean = {
+        of match {
+          case t: PrimitiveType => return true
+          case UserDefinedType(_,_,isStructType) => return isStructType
+          case _ => return false
+        }
+      }
+    }
+    case class Map(keyType: PrimitiveType, valueType: FieldType) extends ParameterizedType(List[FieldType](keyType, valueType)) with BuiltInType
+    
+    
+  }
+  
+  
+  case class FieldDeclaration(fieldtype: FieldType, name: String, index: Int) {
+    override def toString(): String = {
+      return s"$fieldtype $name = $index"
+    }
+  }
+  
+  case class StructFieldDeclaration(fieldtype: FieldType, name: String) {
+    override def toString(): String = {
+      return s"$fieldtype $name"
+    }
+  }
+  
+  abstract class ObjectDeclaration(val name: String) {
+    var packageName: String = ""; // Filled in later
+    def fullyQualifiedName(packageDeclaration: Option[PackageDeclaration]): String = {
+      packageDeclaration match {
+        case Some(decl) => return fullyQualifiedName(decl)
+        case None => return name
+      }
+    }
+    def fullyQualifiedName(packageDeclaration: PackageDeclaration): String = {
+      return packageDeclaration.getFullyQualifiedBagName(name)
+    }
+  }
+  
+  case class BagDeclaration(override val name: String, fields: Seq[FieldDeclaration]) extends ObjectDeclaration(name) {
+    fields.sortWith(_.index < _.index)
+    
+    def getFieldDeclarations(): java.util.List[FieldDeclaration] = {
+      return fields.asJava
+    }
+  }
+  
+  case class StructDeclaration(override val name: String, fields: Seq[StructFieldDeclaration]) extends ObjectDeclaration(name) {
+    def getFieldDeclarations(): java.util.List[StructFieldDeclaration] = {
+      return fields.asJava
+    }
+  }
+  
+  case class PackageDeclaration(packageName: Seq[String]) {
+    def getPackageName(): java.util.List[String] = {
+      return packageName.asJava
+    }
+    def getPackageNameString(): String = {
+      return packageName.mkString(".")
+    }
+    def getFullyQualifiedBagName(bagName: String): String = {
+      return (packageName + bagName).mkString(".")
+    }
+  }
+  
+  case class ImportDeclaration(filename: String)
+  
+  case class BaggageBuffersDeclaration(packageDeclaration: Option[PackageDeclaration], imports: Seq[ImportDeclaration], objectDeclarations: Seq[ObjectDeclaration]) {
+    val bagDeclarations: Seq[BagDeclaration] = objectDeclarations.filter(_.isInstanceOf[BagDeclaration]).map(_.asInstanceOf[BagDeclaration])
+    val structDeclarations: Seq[StructDeclaration] = objectDeclarations.filter(_.isInstanceOf[StructDeclaration]).map(_.asInstanceOf[StructDeclaration])
+    def getObjectDeclarations(): java.util.List[ObjectDeclaration] = {
+      return objectDeclarations.asJava
+    }
+    def getBagDeclarations(): java.util.List[BagDeclaration] = {
+      return bagDeclarations.asJava
+    }
+    def getStructDeclarations(): java.util.List[StructDeclaration] = {
+      return structDeclarations.asJava
+    }
+    def getImportDeclarations(): java.util.List[ImportDeclaration] = {
+      return imports.asJava
+    }
+    def getPackageDeclaration(): PackageDeclaration = {
+      return packageDeclaration.getOrElse(null)
+    }
+    def getPackageNameString(): String = {
+      packageDeclaration match {
+        case Some(decl) => return decl.getPackageNameString()
+        case _ => return ""
+      }
+    }
+    def isEmpty(): Boolean = {
+      return packageDeclaration.isEmpty && imports.isEmpty && bagDeclarations.isEmpty && structDeclarations.isEmpty
+    }
+  }
+  
+}
---
+++ bdl/compiler/src/test/java/brown/tracingplane/bdl/compiler/TestLinker.java
@@ -0,0 +1,298 @@
+package brown.tracingplane.bdl.compiler;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import com.google.common.collect.Lists;
+import com.google.common.io.Files;
+import brown.tracingplane.bdl.compiler.CompileException;
+import brown.tracingplane.bdl.compiler.Linker;
+import brown.tracingplane.bdl.compiler.Ast.BaggageBuffersDeclaration;
+import brown.tracingplane.bdl.compiler.Ast.FieldType;
+import brown.tracingplane.bdl.compiler.Ast.UserDefinedType;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+public class TestLinker {
+    
+    private int nextFileId = 0;
+    
+    final File createFile() throws IOException {
+        File f = File.createTempFile("TestLinker" + nextFileId++, "bb");
+        f.deleteOnExit();
+        return f;
+    }
+    
+    final String create(String contents, Object... formatArgs) throws IOException {
+        File f = createFile();
+        write(f, contents, formatArgs);
+        return f.getAbsolutePath();
+    }
+    
+    final void write(File f, String contents, Object... formatArgs) throws FileNotFoundException {
+        PrintWriter out = new PrintWriter(f);
+        out.write(String.format(contents, formatArgs));
+        out.close();
+    }
+    
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+    
+    @Test
+    public void testEmptyFile() throws IOException, CompileException {
+        String fileName = create("");
+        List<String> inputFiles = Lists.newArrayList(fileName);
+        List<String> bagPath = Lists.newArrayList();
+
+        exception.expect(CompileException.class);
+        Linker.link(inputFiles, bagPath);
+    }
+    
+    @Test
+    public void testSimpleSuccessfulExample() throws IOException, CompileException {
+        String fileName = create("bag MyBag {bool test = 0;}");
+        List<String> inputFiles = Lists.newArrayList(fileName);
+        List<String> bagPath = Lists.newArrayList();
+
+        Linker.link(inputFiles, bagPath);
+    }
+    
+    @Test
+    public void testTwoFiles() throws CompileException, IOException {
+        String first = create("bag MyBag1 {bool test = 0;}");
+        String second = create("bag MyBag2 {bool test = 0;}");
+        List<String> inputFiles = Lists.newArrayList(first, second);
+        List<String> bagPath = Lists.newArrayList();
+
+        Linker.link(inputFiles, bagPath);
+    }
+    
+    @Test
+    public void testTwoFilesAbsoluteImportFailure() throws CompileException, IOException {
+        String first = create("bag MyBag1 {bool test = 0;}");
+        String second = create("import \"%s\";\nbag MyBag2 {bool test = 0;}", first);
+        List<String> inputFiles = Lists.newArrayList(first, second);
+        List<String> bagPath = Lists.newArrayList();
+
+        exception.expect(CompileException.class);
+        Linker.link(inputFiles, bagPath);
+    }
+    
+    @Test
+    public void testTwoFilesRelativeImportSuccess() throws CompileException, IOException {
+        File f1 = createFile();
+        File f2 = createFile();
+        
+        write(f1, "bag MyBag1 {bool test = 0;}");
+        write(f2, "import \"%s\";\nbag MyBag2 {bool test = 0;}", f1.getName());
+
+        List<String> inputFiles = Lists.newArrayList(f1.getAbsolutePath(), f2.getAbsolutePath());
+        List<String> bagPath = Lists.newArrayList();
+
+        Linker.link(inputFiles, bagPath);
+    }
+    
+    @Test
+    public void testTwoFilesRecursiveImport() throws CompileException, IOException {
+        File f1 = createFile();
+        File f2 = createFile();
+        
+        write(f1, "import \"%s\";\nbag MyBag1 {bool test = 0;}", f2.getName());
+        write(f2, "import \"%s\";\nbag MyBag2 {bool test = 0;}", f1.getName());
+
+        List<String> inputFiles = Lists.newArrayList(f1.getAbsolutePath(), f2.getAbsolutePath());
+        List<String> bagPath = Lists.newArrayList();
+
+        //exception.expect(CompileException.class); // recursive import for now allowed
+        Linker.link(inputFiles, bagPath);
+    }
+    
+    @Test
+    public void testTwoFilesBagPathImport() throws CompileException, IOException {
+        File f1 = createFile();
+        File f2 = createFile();
+        
+        write(f1, "bag MyBag1 {bool test = 0;}");
+        write(f2, "import \"%s\";\nbag MyBag2 {bool test = 0;}", f1.getName());
+
+        List<String> inputFiles = Lists.newArrayList(f2.getAbsolutePath());
+        List<String> bagPath = Lists.newArrayList();
+
+        Linker.link(inputFiles, bagPath);
+    }
+    
+    @Test
+    public void testTwoFilesBagPathImportFail() throws CompileException, IOException {
+        
+        File dir = Files.createTempDir();
+        dir.deleteOnExit();
+        
+        File f1 = new File(dir, "bagpathimport");
+        File f2 = createFile();
+        
+        write(f1, "bag MyBag1 {bool test = 0;}");
+        write(f2, "import \"%s\";\nbag MyBag2 {bool test = 0;}", f1.getName());
+
+        List<String> inputFiles = Lists.newArrayList(f2.getAbsolutePath());
+        List<String> bagPath = Lists.newArrayList();
+
+        exception.expect(CompileException.class); // f1 not found on bagpath
+        Linker.link(inputFiles, bagPath);
+    }
+    
+    @Test
+    public void testTwoFilesBagPathImportSuccess() throws CompileException, IOException {
+        
+        File dir = Files.createTempDir();
+        dir.deleteOnExit();
+        
+        File f1 = new File(dir, "bagpathimport");
+        File f2 = createFile();
+        
+        write(f1, "bag MyBag1 {bool test = 0;}");
+        write(f2, "import \"%s\";\nbag MyBag2 {bool test = 0;}", f1.getName());
+
+        List<String> inputFiles = Lists.newArrayList(f2.getAbsolutePath());
+        List<String> bagPath = Lists.newArrayList(dir.getAbsolutePath());
+
+        Linker.link(inputFiles, bagPath);
+    }
+    
+    @Test
+    public void testUserDefinedSameFile() throws CompileException, IOException {
+        File f = createFile();
+        write(f, "bag MyBag1 {bool test = 0;}\nbag MyBag2 {MyBag1 test = 3;}");
+
+        List<String> inputFiles = Lists.newArrayList(f.getAbsolutePath());
+        List<String> bagPath = Lists.newArrayList();
+
+        Linker.link(inputFiles, bagPath);
+    }
+    
+    @Test
+    public void testUserDefinedDifferentFileBothInputs() throws CompileException, IOException {
+        File f1 = createFile();
+        File f2 = createFile();
+        
+        write(f1, "bag MyBag1 {bool test = 0;}");
+        write(f2, "import \"%s\";\nbag MyBag2 {MyBag1 test = 0;}", f1.getName());
+
+        List<String> inputFiles = Lists.newArrayList(f1.getAbsolutePath(), f2.getAbsolutePath());
+        List<String> bagPath = Lists.newArrayList();
+
+        Linker.link(inputFiles, bagPath);
+    }
+    
+    @Test
+    public void testUserDefinedImport() throws CompileException, IOException {
+        File dir = Files.createTempDir();
+        dir.deleteOnExit();
+        
+        File f1 = new File(dir, "bagpathimport");
+        File f2 = createFile();
+        
+        write(f1, "bag MyBag1 {bool test = 0;}");
+        write(f2, "import \"%s\";\nbag MyBag2 {MyBag1 test = 0;}", f1.getName());
+
+        List<String> inputFiles = Lists.newArrayList(f2.getAbsolutePath());
+        List<String> bagPath = Lists.newArrayList(dir.getAbsolutePath());
+
+        Linker.link(inputFiles, bagPath);
+    }
+    
+    @Test
+    public void testUserDefinedImportWithPackage() throws CompileException, IOException {
+        File dir = Files.createTempDir();
+        dir.deleteOnExit();
+        
+        File f1 = new File(dir, "bagpathimport");
+        File f2 = createFile();
+        
+        write(f1, "package edu.brown.cs;\nbag MyBag1 {bool test = 0;}");
+        write(f2, "import \"%s\";\nbag MyBag2 {MyBag1 test = 0;}", f1.getName());
+
+        List<String> inputFiles = Lists.newArrayList(f2.getAbsolutePath());
+        List<String> bagPath = Lists.newArrayList(dir.getAbsolutePath());
+
+        Linker.link(inputFiles, bagPath);
+    }
+    
+    @Test
+    public void testFullyQualifiedUserDefinedImport() throws CompileException, IOException {
+        File dir = Files.createTempDir();
+        dir.deleteOnExit();
+        
+        File f1 = new File(dir, "bagpathimport");
+        File f2 = createFile();
+        
+        write(f1, "package edu.brown.cs;\nbag MyBag1 {bool test = 0;}");
+        write(f2, "import \"%s\";\nbag MyBag2 {edu.brown.cs.MyBag1 test = 0;}", f1.getName());
+
+        List<String> inputFiles = Lists.newArrayList(f2.getAbsolutePath());
+        List<String> bagPath = Lists.newArrayList(dir.getAbsolutePath());
+
+        Linker.link(inputFiles, bagPath);
+    }
+    
+    @Test
+    public void testUserDefinedDoesNotExist() throws CompileException, IOException {
+        File f1 = createFile();
+        File f2 = createFile();
+        
+        write(f1, "bag MyBag1 {bool test = 0;}");
+        write(f2, "import \"%s\";\nbag MyBag2 {MyBag3 test = 0;}", f1.getName());
+
+        List<String> inputFiles = Lists.newArrayList(f1.getAbsolutePath(), f2.getAbsolutePath());
+        List<String> bagPath = Lists.newArrayList();
+
+        exception.expect(CompileException.class); // no such bag MyBag3
+        Linker.link(inputFiles, bagPath);
+    }
+    
+    @Test
+    public void testFullyQualifiedUserDefinedDoesNotExist() throws CompileException, IOException {
+        File f1 = createFile();
+        File f2 = createFile();
+        
+        write(f1, "bag MyBag1 {bool test = 0;}");
+        write(f2, "import \"%s\";\nbag MyBag2 {bad.MyBag1 test = 0;}", f1.getName());
+
+        List<String> inputFiles = Lists.newArrayList(f1.getAbsolutePath(), f2.getAbsolutePath());
+        List<String> bagPath = Lists.newArrayList();
+
+        exception.expect(CompileException.class); // no such bag bad.MyBag1
+        Linker.link(inputFiles, bagPath);
+    }
+
+    
+    @Test
+    public void testPackageResolvesCorrectly() throws CompileException, IOException {
+        File dir = Files.createTempDir();
+        dir.deleteOnExit();
+        
+        File f1 = new File(dir, "bagpathimport");
+        File f2 = createFile();
+        
+        write(f1, "package edu.brown.cs;\nbag MyBag1 {bool test = 0;}");
+        write(f2, "import \"%s\";\nbag MyBag2 {MyBag1 test = 0;}", f1.getName());
+
+        List<String> inputFiles = Lists.newArrayList(f2.getAbsolutePath());
+        List<String> bagPath = Lists.newArrayList(dir.getAbsolutePath());
+
+        Set<BaggageBuffersDeclaration> processed = Linker.link(inputFiles, bagPath);
+        BaggageBuffersDeclaration decl = processed.iterator().next();
+        FieldType ft = decl.getBagDeclarations().get(0).getFieldDeclarations().get(0).fieldtype();
+        assertTrue(ft instanceof UserDefinedType);
+        UserDefinedType uft = (UserDefinedType) ft;
+        assertTrue(uft.hasPackageName());
+        assertEquals("edu.brown.cs", uft.packageName());
+    }
+
+}
