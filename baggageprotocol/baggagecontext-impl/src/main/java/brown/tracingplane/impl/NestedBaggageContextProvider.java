24bd21369321a014fab9263187bb76334b20c24a
Jonathan Mace <jonathan.c.mace@gmail.com>
2017-05-15 12:41:37 -0400

    Add a simple baggagecontext impl for the baggage layer.  Not intended to be used, more as a demonstration, so it's left untested...

---
+++ baggageprotocol/baggagecontext-impl/src/main/java/brown/tracingplane/impl/NestedBaggageContextProvider.java
@@ -0,0 +1,81 @@
+package brown.tracingplane.impl;
+
+import java.nio.ByteBuffer;
+import java.util.List;
+import brown.tracingplane.BaggageContext;
+import brown.tracingplane.BaggageProvider;
+import brown.tracingplane.atomlayer.AtomLayerSerialization;
+import brown.tracingplane.baggageprotocol.BaggageReader;
+import brown.tracingplane.baggageprotocol.BaggageWriter;
+
+/**
+ * <p>
+ * An implementation of {@link BaggageContext} based on atoms and lexicographic merge. {@link AtomContext} represents
+ * the minimal logic necessary to propagate {@link BaggageContext}s and participate in the tracing plane.
+ * </p>
+ */
+public class NestedBaggageContextProvider implements BaggageProvider<NestedBaggageContext> {
+
+    @Override
+    public boolean isValid(BaggageContext baggage) {
+        return baggage == null || baggage instanceof NestedBaggageContext;
+    }
+
+    @Override
+    public NestedBaggageContext newInstance() {
+        return null;
+    }
+
+    @Override
+    public void discard(NestedBaggageContext baggage) {}
+
+    @Override
+    public NestedBaggageContext branch(NestedBaggageContext from) {
+        return from == null ? null : from.branch();
+    }
+
+    @Override
+    public NestedBaggageContext join(NestedBaggageContext left, NestedBaggageContext right) {
+        if (left == null) {
+            return right;
+        } else {
+            left.mergeWith(right);
+            return left;
+        }
+    }
+
+    @Override
+    public NestedBaggageContext deserialize(byte[] serialized, int offset, int length) {
+        List<ByteBuffer> atoms = AtomLayerSerialization.deserialize(serialized, offset, length);
+        return NestedBaggageContext.parse(BaggageReader.create(atoms));
+    }
+
+    @Override
+    public NestedBaggageContext deserialize(ByteBuffer buf) {
+        List<ByteBuffer> atoms = AtomLayerSerialization.deserialize(buf);
+        return NestedBaggageContext.parse(BaggageReader.create(atoms));
+    }
+
+    @Override
+    public byte[] serialize(NestedBaggageContext baggage) {
+        if (baggage == null) {
+            return null;
+        }
+        BaggageWriter writer = BaggageWriter.create();
+        baggage.serialize(writer);
+        List<ByteBuffer> atoms = writer.atoms();
+        return AtomLayerSerialization.serialize(atoms);
+    }
+
+    @Override
+    public byte[] serialize(NestedBaggageContext baggage, int maximumSerializedSize) {
+        if (baggage == null) {
+            return null;
+        }
+        BaggageWriter writer = BaggageWriter.create();
+        baggage.serialize(writer);
+        List<ByteBuffer> atoms = writer.atoms();
+        return AtomLayerSerialization.serialize(atoms, maximumSerializedSize);
+    }
+
+}
