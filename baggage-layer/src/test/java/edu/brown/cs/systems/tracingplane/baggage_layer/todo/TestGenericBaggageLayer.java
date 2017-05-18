ae56fce48d2f471256d8741f6eb6de2f3c562ad3
Jonathan Mace <jonathan.c.mace@gmail.com>
2017-05-18 12:25:07 -0400

    Remove old code

--- baggage-layer/src/test/java/edu/brown/cs/systems/tracingplane/baggage_layer/todo/TestGenericBaggageLayer.java
+++
@@ -1,5 +0,0 @@
-package edu.brown.cs.systems.tracingplane.baggage_layer.todo;
-
-public class TestGenericBaggageLayer {
-
-}
--- baggage-layer/src/test/java/edu/brown/cs/systems/tracingplane/baggage_layer/todo/TestReaderWriterInvalidAtoms.java
+++
@@ -1,7 +0,0 @@
-package edu.brown.cs.systems.tracingplane.baggage_layer.todo;
-
-// TODO:
-
-public class TestReaderWriterInvalidAtoms {
-
-}
